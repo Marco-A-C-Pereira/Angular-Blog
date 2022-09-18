185bd6ce4892c450249586f712cfdf5354e518c2
unknown <finfokka@gmail.com>
2022-09-18 15:39:09 -0300

    Update

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -10,6 +10,8 @@ export class BigCardComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
   }
+
 
 }
---
+++ src/app/components/interfaces/materia.interface.ts
@@ -0,0 +1,6 @@
+export interface Materia {
+  titulo: string
+  materia: string
+  img: string
+  data ?:Date
+}
